aa74772fc164f565c20744f0a8b0cc7fde18d110
LHFY <38107178+lhfy1990@users.noreply.github.com>
2018-05-14 10:10:02 -0400

    Initial Application

---
+++ README.md
@@ -0,0 +1,20 @@
+# Node Express RESTful Example
+An example FullStack RESTful Design using NodeJS and Express. Front-end takes Angular but not necessary.
+
+## Initialization
+1. Make sure you have node installed with npm.
+2. Make sure you have bower installed.
+  * If not, run `npm install -g bower` to install.
+3. Run `npm install` under the repository folder
+  * It will handle node modules and bower modules.
+  * Read .bowerrc and package.json for detail.
+
+## Files
+* server.js: main server.
+
+## References:
+* [REST API Tutorial](http://www.restapitutorial.com/)
+* [Using HTTP Methods for RESTful Services](http://www.restapitutorial.com/lessons/httpmethods.html)
+* [REST API Quick Tips](http://www.restapitutorial.com/lessons/restquicktips.html)
+* [Build a RESTful API Using Node and Express 4](https://scotch.io/tutorials/build-a-restful-api-using-node-and-express-4)
+* [Dynamically Loading Controllers and Views with AngularJS/$controllerProvider and RequireJS](https://weblogs.asp.net/dwahlin/dynamically-loading-controllers-and-views-with-angularjs-and-requirejs)
---
+++ server.js
@@ -0,0 +1,39 @@
+// requires
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var mongoose = require('mongoose');
+
+// configs
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+var port = process.env.PORT || 8080;
+
+// mongoose.set('debug', true);
+var mongodb = 'mongodb://localhost/app';
+// var mongodb = 'mongodb://user:pass@mongodb/app';
+mongoose.connect(mongodb, { autoIndex: false });
+var db = mongoose.connection;
+var referenceModel = require('./models/reference.js')(db);
+var objectModel = require('./models/object.js')(db);
+
+// execution
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    objectModel.ensureIndexes();
+    referenceModel.ensureIndexes();
+    console.log(`${mongodb} connect`);
+    // route
+    // static
+    app.use('', express.static(path.join(__dirname, 'public')));
+    // api
+    router_api = require('./routes/api.js');
+    app.use('/api', router_api);
+
+    // execute
+    app.listen(port);
+    console.log(`Server running on port: ${port}`);
+});
